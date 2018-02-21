3b6b5a5f0468141d56895284e56bc5a417723aee
ScorpioMiku <1056992492@qq.com>
2018-02-21 16:00:50 +0800

    ok

---
+++ mnisteval.py
@@ -0,0 +1,69 @@
+import os
+import os.path
+import math
+import numpy as np
+import tensorflow as tf
+import cifar10_input
+import function
+import net
+
+
+BATCH_SIZE = 32
+def evaluate():
+    with tf.Graph().as_default():
+
+        log_dir = 'F:/Traindata/MNIST_pictures/result'
+        test_dir = "F:/Traindata/MNIST_pictures/MNISTTest.tfrecords"
+        n_test = 1000
+
+        # reading test data
+        images, labels = function.readDataFromTF(test_dir,BATCH_SIZE,shuffle=False)
+
+        #logits = net.inference(images)
+        logits = net.net(images, batch_size=BATCH_SIZE, num_class=10, keep_prob=0.5, name="train")
+        print("labels", labels)
+        print("logits", logits)
+        accuracy221 = function.accuracy22(logits, labels)
+        print(accuracy221)
+        saver = tf.train.Saver(tf.global_variables())
+
+        with tf.Session() as sess:
+
+            print("Reading checkpoints...")
+            ckpt = tf.train.get_checkpoint_state(log_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                print('Loading success, global_step is %s' % global_step)
+            else:
+                print('No checkpoint file found')
+                return
+
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+            accuracy2212 = sess.run([accuracy221])
+            accuracy2212 = accuracy2212[0]
+            try:
+                num_iter = int(math.ceil(n_test / BATCH_SIZE))
+                true_count = 0
+                total_sample_count = num_iter * BATCH_SIZE
+                step = 0
+
+                while step < num_iter and not coord.should_stop():
+
+                    #true_count += np.sum(predictions)
+                    step += 1
+                    #precision = true_count / total_sample_count
+                print(accuracy2212)
+                print('accuracy22: %.4f%% *****'
+                      % (accuracy2212))
+                #print('precision = %.3f' % )
+            except Exception as e:
+                coord.request_stop(e)
+            finally:
+                coord.request_stop()
+                coord.join(threads)
+
+
+# %%
+evaluate()
--- train.py
+++ train.py
@@ -5,11 +5,11 @@ import net
 import function
 
 n_class = 10
-batch_size = 27
+batch_size = 32
 img_w = 28
 img_h = 28
-LEARNING_RATE = 0.0001
-MAX_STEP = 2000
+LEARNING_RATE = 0.001
+MAX_STEP = 5000
 
 CAPACITY = 2000
 
@@ -18,65 +18,63 @@ TEST_FILENAME = "F:/Traindata/MNIST_pictures/MNISTTest.tfrecords"
 
 TRAIN_RESULTS_FILENAME = "F:/Traindata/MNIST_pictures/result"
 
-#1
+
+# 1
 def train():
-    with tf.variable_scope("input"):
-        train_image_batch, train_label_batch = function.readDataFromTF(TRAIN_FILENAME, batch_size=batch_size)
-        test_image_batch, test_label_batch = function.readDataFromTF(TEST_FILENAME, batch_size=batch_size)
+    train_image_batch, train_label_batch = function.readDataFromTF(TRAIN_FILENAME, batch_size=batch_size)
 
-        logits = net.net(train_image_batch, batch_size=batch_size, num_class=n_class, keep_prob=0.5, name="train")
-        train_logits = net.net(train_image_batch, batch_size=batch_size, num_class=n_class, keep_prob=1.0,
-                               name="train_accuracy")
-        test_logits = net.net(test_image_batch, batch_size=batch_size, num_class=n_class, keep_prob=1.0,
-                              name='test_accuracy')
-        print("int?",train_label_batch)
-        loss = function.losses(logits=logits, labels=train_label_batch)
-        #loss = tf.reduce_mean(-tf.reduce_sum(train_label_batch * tf.log(logits), reduction_indices=[1]))
-        train_accuracy = function.accuracy(train_logits, train_label_batch)
-        test_accuracy = function.accuracy(test_logits, test_label_batch)
+    logits = net.net(train_image_batch, batch_size=batch_size, num_class=n_class, keep_prob=0.5, name="train")
+    #train_logits = net.net(train_image_batch, batch_size=batch_size, num_class=n_class, keep_prob=1.0,name="train_accuracy")
+    train_logits = net.inference(train_image_batch)
 
-        my_global_step = tf.Variable(0, name='global_step')
-        train_op = function.optimize(loss=loss, learning_rate=LEARNING_RATE, global_step=my_global_step)
-        #train_op = tf.train.AdamOptimizer(1e-4).minimize(loss)
-        saver = tf.train.Saver(tf.global_variables())
-        summary_op = tf.summary.merge_all()
+    print("int?", train_label_batch)
+    loss = function.losses(logits=logits, labels=train_label_batch)
+    # loss = tf.reduce_mean(-tf.reduce_sum(train_label_batch * tf.log(logits), reduction_indices=[1]))
+    #train_accuracy = function.accuracy(train_logits, train_label_batch)
 
-        init = tf.global_variables_initializer()
+    my_global_step = tf.Variable(0, name='global_step')
+    optimizer = tf.train.GradientDescentOptimizer(LEARNING_RATE)
+    train_op = optimizer.minimize(loss, global_step=my_global_step)
+    # train_op = tf.train.AdamOptimizer(1e-4).minimize(loss)
+    saver = tf.train.Saver(tf.global_variables())
+    summary_op = tf.summary.merge_all()
 
-        sess = tf.Session()
-        sess.run(init)
+    init = tf.global_variables_initializer()
 
-        coord = tf.train.Coordinator()
-        threads = tf.train.start_queue_runners(sess=sess, coord=coord)
+    sess = tf.Session()
+    sess.run(init)
 
-        tra_summary_writer = tf.summary.FileWriter(TRAIN_RESULTS_FILENAME, sess.graph)
+    coord = tf.train.Coordinator()
+    threads = tf.train.start_queue_runners(sess=sess, coord=coord)
 
-        try:
-            for step in np.arange(MAX_STEP):
-                if coord.should_stop():
-                    break
+    tra_summary_writer = tf.summary.FileWriter(TRAIN_RESULTS_FILENAME, sess.graph)
 
-                _, train_loss = sess.run([train_op, loss])
+    try:
+        for step in np.arange(MAX_STEP):
+            if coord.should_stop():
+                break
 
-                if (step % 50 == 0) or (step == MAX_STEP):
-                    print('***** Step: %d, loss: %.4f' % (step, train_loss))
-                    summary_str = sess.run(summary_op)
-                    tra_summary_writer.add_summary(summary_str, step)
-                if (step % 200 == 0) or (step == MAX_STEP):
-                    testaccuracy, trainaccuracy = sess.run([test_accuracy, train_accuracy])
-                    print('***** Step: %d, loss: %.4f, test Set accuracy: %.4f%% ,train Set accuracy: %.4f%% *****'
-                          % (step, train_loss, testaccuracy, trainaccuracy))
-                    summary_str = sess.run(summary_op)
-                    tra_summary_writer.add_summary(summary_str, step)
-                if step % 2000 == 0 or step == MAX_STEP:
-                    checkpoint_path = os.path.join(TRAIN_RESULTS_FILENAME, 'model.ckpt')
-                    saver.save(sess, checkpoint_path, global_step=step)
-        except tf.errors.OutOfRangeError:
-            print('error')
-        finally:
-            coord.request_stop()
-        coord.join(threads)
-        sess.close()
+            _, train_loss = sess.run([train_op, loss])
+
+            if (step % 50 == 0) or (step == MAX_STEP):
+                print('***** Step: %d, loss: %.4f' % (step, train_loss))
+                summary_str = sess.run(summary_op)
+                tra_summary_writer.add_summary(summary_str, step)
+            if (step % 200 == 0) or (step == MAX_STEP):
+                #trainaccuracy = sess.run([train_accuracy])
+                print('***** Step: %d, loss: %.4f*****'
+                      % (step, train_loss))
+                summary_str = sess.run(summary_op)
+                tra_summary_writer.add_summary(summary_str, step)
+            if step % 2000 == 0 or step == MAX_STEP:
+                checkpoint_path = os.path.join(TRAIN_RESULTS_FILENAME, 'model.ckpt')
+                saver.save(sess, checkpoint_path, global_step=step)
+    except tf.errors.OutOfRangeError:
+        print('error')
+    finally:
+        coord.request_stop()
+    coord.join(threads)
+    sess.close()
 
 
 train()
